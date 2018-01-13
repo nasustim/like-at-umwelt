e84fa0e99839b7337e03a4d7c96c25be559785af
Mitsuhiro HIBINO <hibino17@iamas.ac.jp>
2018-01-13 15:49:32 +0900

    index.html: coding fronte-end

--- srv.py
+++ srv.py
@@ -1,11 +1,21 @@
 # coding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 app = Flask(__name__)
+
+store = 0
 
 @app.route("/")
 def root():
 	return render_template("index.html")
+
+@app.route("/signage")
+def signage():
+	return render_template("signage.html")
+
+@app.route("/post",methods=['POST'])
+def post():
+	get_json = request.json
 
 def main():
 	app.run()
